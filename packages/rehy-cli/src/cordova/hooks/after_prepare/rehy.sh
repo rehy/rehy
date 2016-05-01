cac48a3d2ebd55b1dc03afa65e5a9793ee45f34d
Ratson <github@ratson.name>
2016-05-01 22:41:30 +0800

    Copy Android res

--- packages/rehy-cli/src/cordova/hooks/after_prepare/rehy.sh
+++ packages/rehy-cli/src/cordova/hooks/after_prepare/rehy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir -p platforms/android/res/drawable-xxhdpi platforms/android/res/drawable-xxxhdpi platforms/android/res/drawable
+
+ANDROID_RES_DIR=$INIT_CWD/cordova/android/res/
+test -d $ANDROID_RES_DIR && cpy '**/*.*' $PWD/platforms/android/res/ --cwd=$ANDROID_RES_DIR --parents
+
 cordova-icon
 cordova-splash
