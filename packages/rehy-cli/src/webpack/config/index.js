bf92f45dfb3620e80e52ddce048c09114779c4c1
Ratson <github@ratson.name>
2016-05-01 23:51:09 +0800

    Validate webpack config

--- packages/rehy-cli/src/webpack/config/index.js
+++ packages/rehy-cli/src/webpack/config/index.js
@@ -1,17 +1,18 @@
 import path from 'path'
 
+import validate from 'webpack-validator'
+
 import webpackConfigBase from 'rehy-webpack-config'
 
 const webpackOutputPath = '.rehy/local/webpack-output'
 
-export default webpackConfigBase
-  .extend({
-    [path.resolve(__dirname, './[env].js')]: ({default: config}) => {
-      return config
-    },
-  })
-  .merge({
-    output: {
-      path: path.join(process.cwd(), webpackOutputPath),
-    },
-  })
+export default validate(webpackConfigBase.extend({
+  [path.resolve(__dirname, './[env].js')]: ({default: config}) => {
+    return config
+  },
+})
+.merge({
+  output: {
+    path: path.join(process.cwd(), webpackOutputPath),
+  },
+}))
