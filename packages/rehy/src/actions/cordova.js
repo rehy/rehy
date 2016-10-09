264899592d6a61860b1369b57a0162f4f72b139e
Ratson <github@ratson.name>
2016-10-09 23:29:53 +0800

    Export listenToDeviceReady()

--- packages/rehy/src/actions/cordova.js
+++ packages/rehy/src/actions/cordova.js
@@ -1,6 +1,15 @@
 /* global cordova */
+import { listenToEvent } from './event'
+
 export const handleDeviceReady = () => () => {
   if (navigator.splashscreen) {
     navigator.splashscreen.hide()
   }
+}
+
+export function listenToDeviceReady(action) {
+  return listenToEvent('deviceready', action, {
+    args: [false],
+    target: document,
+  })
 }
--- packages/rehy/src/entry/cordova.js
+++ packages/rehy/src/entry/cordova.js
@@ -1,9 +1,5 @@
-import { handleDeviceReady } from '../actions/cordova'
-import { listenToEvent } from '../actions/event'
+import { handleDeviceReady, listenToDeviceReady } from '../actions/cordova'
 
 export default ({ store }) => {
-  store.dispatch(listenToEvent('deviceready', handleDeviceReady, {
-    args: [false],
-    target: document,
-  }))
+  store.dispatch(listenToDeviceReady(handleDeviceReady))
 }
